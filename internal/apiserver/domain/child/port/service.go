dd9c016c9184aec92776955f6454e7c195ccad6a
yshujie <yshujie@163.com>
2025-10-12 17:15:30 +0800

    pref 重构 Child 应用服务定义，增加 ctx 参数

--- internal/apiserver/domain/child/port/service.go
+++ internal/apiserver/domain/child/port/service.go
@@ -1,24 +1,26 @@
 package port
 
 import (
+	"context"
+
 	"github.com/fangcun-mount/iam-contracts/internal/apiserver/domain/child"
 	"github.com/fangcun-mount/iam-contracts/internal/pkg/meta"
 )
 
 // ChildRegister 儿童档案注册服务接口
 type ChildRegister interface {
-	Register(name string, gender meta.Gender, birthday meta.Birthday) (child *child.Child, err error)
+	Register(ctx context.Context, name string, gender meta.Gender, birthday meta.Birthday) (child *child.Child, err error)
 }
 
 // ChildProfileEditor 儿童档案编辑服务接口
 type ChildProfileEditor interface {
-	Rename(childID child.ChildID, name string) error
-	UpdateIDCard(childID child.ChildID, idCard meta.IDCard) error
-	UpdateProfile(childID child.ChildID, gender meta.Gender, birthday meta.Birthday) error
-	UpdateHeightWeight(childID child.ChildID, height meta.Height, weight meta.Weight) error
+	Rename(ctx context.Context, childID child.ChildID, name string) error
+	UpdateIDCard(ctx context.Context, childID child.ChildID, idCard meta.IDCard) error
+	UpdateProfile(ctx context.Context, childID child.ChildID, gender meta.Gender, birthday meta.Birthday) error
+	UpdateHeightWeight(ctx context.Context, childID child.ChildID, height meta.Height, weight meta.Weight) error
 }
 
 // SimilarChildFinder 相似儿童档案查找服务接口
 type SimilarChildFinder interface {
-	FindChilds(name string, gender meta.Gender, birthday meta.Birthday) (children []*child.Child, err error)
+	FindChilds(ctx context.Context, name string, gender meta.Gender, birthday meta.Birthday) (children []*child.Child, err error)
 }
