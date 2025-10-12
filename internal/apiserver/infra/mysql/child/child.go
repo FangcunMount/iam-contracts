eb8cd9c28ecac5d5d44fc6fd79914877e58d6332
yshujie <yshujie@163.com>
2025-10-12 16:51:10 +0800

    feat 实现 child 仓储层

---
+++ internal/apiserver/infra/mysql/child/child.go
@@ -0,0 +1,46 @@
+package child
+
+import (
+	"time"
+
+	base "github.com/fangcun-mount/iam-contracts/internal/apiserver/infra/mysql"
+	"github.com/fangcun-mount/iam-contracts/pkg/util/idutil"
+	"gorm.io/gorm"
+)
+
+// ChildPO 儿童档案持久化对象
+// 对应数据库表结构
+type ChildPO struct {
+	base.AuditFields
+	Name     string `gorm:"column:name;type:varchar(64);not null;comment:儿童姓名"`
+	IDCard   string `gorm:"column:id_card;type:varchar(20);uniqueIndex;comment:身份证号码"`
+	Gender   uint8  `gorm:"column:gender;type:tinyint;not null;default:0;comment:性别"`
+	Birthday string `gorm:"column:birthday;type:varchar(10);comment:出生日期"`
+	Height   int64  `gorm:"column:height;type:bigint;comment:身高(以0.1cm为单位)"`
+	Weight   int64  `gorm:"column:weight;type:bigint;comment:体重(以0.1kg为单位)"`
+}
+
+// TableName 指定表名
+func (ChildPO) TableName() string {
+	return "children"
+}
+
+// BeforeCreate 在创建前设置信息
+func (p *ChildPO) BeforeCreate(tx *gorm.DB) error {
+	p.ID = idutil.NewID(idutil.GetIntID())
+	p.CreatedAt = time.Now()
+	p.UpdatedAt = time.Now()
+	p.CreatedBy = idutil.NewID(0)
+	p.UpdatedBy = idutil.NewID(0)
+	p.DeletedBy = idutil.NewID(0)
+
+	return nil
+}
+
+// BeforeUpdate 在更新前设置信息
+func (p *ChildPO) BeforeUpdate(tx *gorm.DB) error {
+	p.UpdatedAt = time.Now()
+	p.UpdatedBy = idutil.NewID(0)
+
+	return nil
+}
---
+++ internal/apiserver/infra/mysql/child/mapper.go
@@ -0,0 +1,82 @@
+package child
+
+import (
+	domain "github.com/fangcun-mount/iam-contracts/internal/apiserver/domain/child"
+	"github.com/fangcun-mount/iam-contracts/internal/pkg/meta"
+)
+
+// ChildMapper 儿童档案映射器
+// 负责领域模型与持久化对象之间的转换
+type ChildMapper struct{}
+
+// NewChildMapper 创建儿童档案映射器
+func NewChildMapper() *ChildMapper {
+	return &ChildMapper{}
+}
+
+// ToPO 将领域模型转换为持久化对象
+func (m *ChildMapper) ToPO(cBO *domain.Child) *ChildPO {
+	if cBO == nil {
+		return nil
+	}
+
+	po := &ChildPO{
+		Name:     cBO.Name,
+		IDCard:   cBO.IDCard.String(),
+		Gender:   cBO.Gender.Value(),
+		Birthday: cBO.Birthday.String(),
+		Height:   cBO.Height.Tenths(),
+		Weight:   cBO.Weight.Tenths(),
+	}
+
+	po.ID = cBO.ID
+
+	return po
+}
+
+// ToBO 将持久化对象转换为领域模型
+func (m *ChildMapper) ToBO(po *ChildPO) *domain.Child {
+	if po == nil {
+		return nil
+	}
+
+	child := &domain.Child{
+		ID:       domain.ChildID(po.ID),
+		Name:     po.Name,
+		IDCard:   meta.NewIDCard(po.Name, po.IDCard),
+		Gender:   meta.NewGender(po.Gender),
+		Birthday: meta.NewBirthday(po.Birthday),
+		Height:   meta.NewHeightFromTenths(po.Height),
+		Weight:   meta.NewWeightFromTenths(po.Weight),
+	}
+
+	return child
+}
+
+// ToBOs 将持久化对象列表转换为领域模型列表
+func (m *ChildMapper) ToBOs(pos []*ChildPO) []*domain.Child {
+	if pos == nil {
+		return nil
+	}
+
+	var bos []*domain.Child
+	for _, po := range pos {
+		bos = append(bos, m.ToBO(po))
+	}
+
+	return bos
+}
+
+// ToPOs 将领域模型列表转换为持久化对象列表
+func (m *ChildMapper) ToPOs(bos []*domain.Child) []*ChildPO {
+	if bos == nil {
+		return nil
+	}
+
+	var pos []*ChildPO
+	for _, bo := range bos {
+		pos = append(pos, m.ToPO(bo))
+	}
+
+	return pos
+}
