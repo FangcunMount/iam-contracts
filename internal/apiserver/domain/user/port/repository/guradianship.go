be077ecf5e769ea0d981a3f377a94e07905e0cd1
yshujie <yshujie@163.com>
2025-10-12 14:39:08 +0800

    feat 定义「用户模块」port

---
+++ internal/apiserver/domain/user/port/repository/guradianship.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/fangcun-mount/iam-contracts/internal/apiserver/domain/user"
+	"github.com/fangcun-mount/iam-contracts/internal/pkg/meta"
+)
+
+// GuardianshipRepository 监护关系存储接口
+type GuardianshipRepository interface {
+	Create(ctx context.Context, guardianship user.Guardianship) error
+	FindByID(ctx context.Context, id meta.ID) (user.Guardianship, error)
+	FindByChildID(ctx context.Context, id user.ChildID) (guardianships []user.Guardianship, err error)
+	FindByUserID(ctx context.Context, id user.UserID) (guardianships []user.Guardianship, err error)
+	Update(ctx context.Context, guardianship user.Guardianship) error
+}
