bc5ad4069a23f248eb1de2f00b168cf54161ac48
yshujie <yshujie@163.com>
2025-10-13 11:51:41 +0800

    fix 更新 grpc 接口

--- internal/apiserver/interface/grpc/pb/iam/identity/v1/identity.pb.go
+++ internal/apiserver/interface/grpc/pb/iam/identity/v1/identity.pb.go
@@ -750,7 +750,7 @@ const file_iam_identity_v1_identity_proto_rawDesc = "" +
 	"\x11GuardianshipQuery\x12M\n" +
 	"\n" +
 	"IsGuardian\x12\x1e.iam.identity.v1.IsGuardianReq\x1a\x1f.iam.identity.v1.IsGuardianResp\x12S\n" +
-	"\fListChildren\x12 .iam.identity.v1.ListChildrenReq\x1a!.iam.identity.v1.ListChildrenRespBLZJgithub.com/fangcun-mount/iam-contracts/api/grpc/iam/identity/v1;identityv1b\x06proto3"
+	"\fListChildren\x12 .iam.identity.v1.ListChildrenReq\x1a!.iam.identity.v1.ListChildrenRespBdZbgithub.com/fangcun-mount/iam-contracts/internal/apiserver/interface/grpc/pb/identity/v1;identityv1b\x06proto3"
 
 var (
 	file_iam_identity_v1_identity_proto_rawDescOnce sync.Once
