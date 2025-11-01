f5fb65483ce9f856dc2b81f5433bb370d4ba1ea9
yshujie <yshujie@163.com>
2025-11-01 15:05:51 +0800

    fix 修复 IDCard 为空时的报错

--- internal/apiserver/modules/uc/infra/mysql/child/child.go
+++ internal/apiserver/modules/uc/infra/mysql/child/child.go
@@ -12,12 +12,13 @@ import (
 // 对应数据库表结构
 type ChildPO struct {
 	base.AuditFields
-	Name     string `gorm:"column:name;type:varchar(64);not null;index:idx_name_gender_birthday,priority:1;comment:儿童姓名"`
-	IDCard   string `gorm:"column:id_card;type:varchar(20);uniqueIndex;comment:身份证号码"`
-	Gender   uint8  `gorm:"column:gender;type:tinyint;not null;default:0;index:idx_name_gender_birthday,priority:2;comment:性别"`
-	Birthday string `gorm:"column:birthday;type:varchar(10);index:idx_name_gender_birthday,priority:3;comment:出生日期"`
-	Height   int64  `gorm:"column:height;type:bigint;comment:身高(以0.1cm为单位)"`
-	Weight   int64  `gorm:"column:weight;type:bigint;comment:体重(以0.1kg为单位)"`
+	Name string `gorm:"column:name;type:varchar(64);not null;index:idx_name_gender_birthday,priority:1;comment:儿童姓名"`
+	// IDCard 是可空的；使用指针以便将空值写入 NULL，避免唯一索引对空字符串的冲突
+	IDCard   *string `gorm:"column:id_card;type:varchar(20);uniqueIndex;comment:身份证号码"`
+	Gender   uint8   `gorm:"column:gender;type:tinyint;not null;default:0;index:idx_name_gender_birthday,priority:2;comment:性别"`
+	Birthday string  `gorm:"column:birthday;type:varchar(10);index:idx_name_gender_birthday,priority:3;comment:出生日期"`
+	Height   int64   `gorm:"column:height;type:bigint;comment:身高(以0.1cm为单位)"`
+	Weight   int64   `gorm:"column:weight;type:bigint;comment:体重(以0.1kg为单位)"`
 }
 
 // TableName 指定表名
